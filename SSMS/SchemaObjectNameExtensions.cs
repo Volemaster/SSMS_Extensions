f4d7b23d64bb69475a653d84d611b4f69a1f7ca4
Courtney White <courtney.mason.white@gmail.com>
2022-09-09 08:53:39 -0400

    Remove old code, add SchemaObjectNameExtensions

---
+++ SSMS/SchemaObjectNameExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System.Text;
+
+namespace SSMS
+{
+    public static class SchemaObjectNameExtensions
+    {
+        public static string QuoteName(this SchemaObjectName objectName)
+        {
+            var c = new CreateAggregateStatement();
+            var sb = new StringBuilder();
+            for (int i = 0; i < objectName.Identifiers.Count; i++)
+            {
+                if (i > 0) sb.Append(".");
+                sb.Append(Identifier.EncodeIdentifier(objectName.Identifiers[i].Value, QuoteType.DoubleQuote));
+            }
+            return sb.ToString();
+        }
+    }
+}
